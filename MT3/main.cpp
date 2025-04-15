5359f2ef483b3a42c7600f7b0a004bbdc78a2a07
ChouNaifu <k024g1023@g.neec.ac.jp>
2025-04-15 11:50:55 +0900

    00_01

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,6 +1,54 @@
 #include <Novice.h>
+#include <math.h>
 
 const char kWindowTitle[] = "GC1B_10_チョウ_ナイーフ_タイトル";
+
+struct Vector3 {
+	float x, y, z;
+};
+
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+}
+
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	return { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+}
+
+// スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v) {
+	return { v.x * scalar, v.y * scalar, v.z * scalar };
+}
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// 長さ
+float Length(const Vector3& v) {
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+// 正規化
+Vector3 Normalize(const Vector3& v) {
+	float length = Length(v);
+	if (length != 0) {
+		return { v.x / length, v.y / length, v.z / length };
+	}
+	return { 0.0f, 0.0f, 0.0f };
+}
+
+// 3次元ベクトルの数値表示
+static const int kColumnWidth = 60;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -12,7 +60,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Vector3 v1{ 1.0f, 3.0f, -5.0f };
+	Vector3 v2{ 4.0f, -1.0f, 2.0f };
+	float k = 4.0f;
 
+	Vector3 resultAdd = Add(v1, v2);
+	Vector3 resultSubtract = Subtract(v1, v2);
+	Vector3 resultMultiply = Multiply(k, v1);
+	float resultDot = Dot(v1, v2);
+	float resultLength = Length(v1);
+	Vector3 resultNormalize = Normalize(v2);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -34,8 +91,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-
+		const int kRowHeight = 20;
+		VectorScreenPrintf(0, 0, resultAdd, " : Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
 		///
 		/// ↑描画処理ここまで
 		///
