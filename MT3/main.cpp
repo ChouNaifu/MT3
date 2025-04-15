53a70d2cbcd8b37939e75ed209c4edba984da50d
ChouNaifu <k024g1023@g.neec.ac.jp>
2025-04-15 12:38:53 +0900

    4/15

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,117 +1,162 @@
 #include <Novice.h>
 #include <math.h>
+#include <assert.h>
 
 const char kWindowTitle[] = "GC1B_10_チョウ_ナイーフ_タイトル";
 
-struct Vector3 {
-	float x, y, z;
+struct Matrix4x4 {
+    float m[4][4];
 };
 
-// 加算
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+// 加法
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+    Matrix4x4 result{};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result.m[i][j] = m1.m[i][j] + m2.m[i][j];
+        }
+    }
+    return result;
 }
 
-// 減算
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-	return { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+// 減法
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+    Matrix4x4 result{};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result.m[i][j] = m1.m[i][j] - m2.m[i][j];
+        }
+    }
+    return result;
 }
 
-// スカラー倍
-Vector3 Multiply(float scalar, const Vector3& v) {
-	return { v.x * scalar, v.y * scalar, v.z * scalar };
+// 積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+    Matrix4x4 result{};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result.m[i][j] = 0.0f;
+            for (int k = 0; k < 4; ++k) {
+                result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+            }
+        }
+    }
+    return result;
 }
 
-// 内積
-float Dot(const Vector3& v1, const Vector3& v2) {
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+// 逆行列
+
+Matrix4x4 Inverse(const Matrix4x4& m) { 
+
+}
+// 転置行列
+Matrix4x4 Transpose(const Matrix4x4& m) {
+    Matrix4x4 result{};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result.m[i][j] = m.m[j][i];
+        }
+    }
+    return result;
 }
 
-// 長さ
-float Length(const Vector3& v) {
-	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+// 単位行列
+Matrix4x4 MakeIdentity4x4() {
+    Matrix4x4 result{};
+    for (int i = 0; i < 4; ++i) {
+        result.m[i][i] = 1.0f;
+    }
+    return result;
 }
 
-// 正規化
-Vector3 Normalize(const Vector3& v) {
-	float length = Length(v);
-	if (length != 0) {
-		return { v.x / length, v.y / length, v.z / length };
-	}
-	return { 0.0f, 0.0f, 0.0f };
-}
-
-// 3次元ベクトルの数値表示
-static const int kColumnWidth = 60;
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+// 数値表示
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+    const int kColumnWidth = 60;
+    const int kRowHeight = 20;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            Novice::ScreenPrintf(x + kColumnWidth * j, y + kRowHeight * i, "%.02f", matrix.m[i][j]);
+        }
+    }
+    Novice::ScreenPrintf(x + kColumnWidth * 4, y, "%s", label);
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+    // ライブラリの初期化
+    Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+    // キー入力結果を受け取る箱
+    char keys[256] = {0};
+    char preKeys[256] = {0};
 
-	Vector3 v1{ 1.0f, 3.0f, -5.0f };
-	Vector3 v2{ 4.0f, -1.0f, 2.0f };
-	float k = 4.0f;
+    Matrix4x4 m1 = {
+        3.2f, 0.7f, 9.6f, 4.4f,
+        5.5f, 1.3f, 7.8f, 2.1f,
+        6.9f, 8.0f, 2.6f, 1.0f,
+        0.5f, 7.2f, 5.1f, 3.3f
+    };
 
-	Vector3 resultAdd = Add(v1, v2);
-	Vector3 resultSubtract = Subtract(v1, v2);
-	Vector3 resultMultiply = Multiply(k, v1);
-	float resultDot = Dot(v1, v2);
-	float resultLength = Length(v1);
-	Vector3 resultNormalize = Normalize(v2);
+    Matrix4x4 m2 = {
+        4.1f, 6.5f, 3.3f, 2.2f,
+        8.8f, 0.6f, 9.9f, 7.7f,
+        1.1f, 5.5f, 6.6f, 0.0f,
+        3.3f, 9.9f, 8.8f, 2.2f
+    };
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+    Matrix4x4 resultAdd = Add(m1, m2);
+    Matrix4x4 resultSubtract = Subtract(m1, m2);
+    Matrix4x4 resultMultiply = Multiply(m1, m2);
+    Matrix4x4 inverseM1 = Inverse(m1);
+    Matrix4x4 inverseM2 = Inverse(m2);
+    Matrix4x4 transposeM1 = Transpose(m1);
+    Matrix4x4 transposeM2 = Transpose(m2);
+    Matrix4x4 identity = MakeIdentity4x4();
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+    // ウィンドウの×ボタンが押されるまでループ
+    while (Novice::ProcessMessage() == 0) {
+        // フレームの開始
+        Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+        // キー入力を受け取る
+        memcpy(preKeys, keys, 256);
+        Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+        ///
+        /// ↓更新処理ここから
+        ///
 
-		///
-		/// ↓描画処理ここから
-		///
-		const int kRowHeight = 20;
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
-		///
-		/// ↑描画処理ここまで
-		///
+        ///
+        /// ↑更新処理ここまで
+        ///
 
-		// フレームの終了
-		Novice::EndFrame();
+        ///
+        /// ↓描画処理ここから
+        ///
+        const int kRowHeight = 100;
+        MatrixScreenPrintf(0, 0, resultAdd, "Add");
+        MatrixScreenPrintf(0, kRowHeight, resultSubtract, "Subtract");
+        MatrixScreenPrintf(0, kRowHeight * 2, resultMultiply, "Multiply");
+        MatrixScreenPrintf(0, kRowHeight * 3, inverseM1, "InverseM1");
+        MatrixScreenPrintf(0, kRowHeight * 4, inverseM2, "InverseM2");
+        MatrixScreenPrintf(0, kRowHeight * 5, transposeM1, "TransposeM1");
+		MatrixScreenPrintf(0, kRowHeight * 6, transposeM2, "TransposeM2");
+        MatrixScreenPrintf(400, 0, identity, "Identity");
+        ///
+        /// ↑描画処理ここまで
+        ///
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+        // フレームの終了
+        Novice::EndFrame();
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+        // ESCキーが押されたらループを抜ける
+        if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+            break;
+        }
+    }
+
+    // ライブラリの終了
+    Novice::Finalize();
+    return 0;
 }
